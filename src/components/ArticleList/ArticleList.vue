82f8ac7a91246c72bc2fd6a8c6141e5bc7b10401
JianChao Wang <781277276@qq.com>
2022-01-11 14:42:21 +0800

    调整图片宽度

--- src/components/ArticleList/ArticleList.vue
+++ src/components/ArticleList/ArticleList.vue
@@ -4,10 +4,14 @@
       <div class="article-title"><b>我的博客</b></div>
       <div class="article-box" v-for="item in 8" :key="item">
         <iv-row>
-          <iv-col span="8">
+          <iv-col span="9">
             <img src="https://img.rosystar.cn/11.png" alt="">
           </iv-col>
-          <iv-col span="16"></iv-col>
+          <iv-col span="15">
+            <iv-row>标题</iv-row>
+            <iv-row>简介</iv-row>
+            <iv-row>标签行</iv-row>
+          </iv-col>
         </iv-row>
       </div>
     </div>
@@ -45,7 +49,7 @@ export default {
 }
 
 .article-box img{
-  width: 280px;
+  width: 100%;
   height: 100%;
 }
 
